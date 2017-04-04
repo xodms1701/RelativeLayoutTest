7875710f95b7fd23436e4979b11d90bab89d07b3
xodms1701 <kimrrrrt123@naver.com>
2017-04-04 19:10:18 +0900

    #1 first RelativeLayout

---
+++ app/src/main/java/org/techtown/relativelayouttest/MainActivity.java
@@ -0,0 +1,18 @@
+package org.techtown.relativelayouttest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.techtown.relativelayouttest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
